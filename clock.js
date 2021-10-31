107921bdf841102de452bd8674357f5200d5cbed
Bharath Raja ツ <2320747+bigomega@users.noreply.github.com>
2021-10-31 20:40:11 +0530

    Adding minutes and seconds on hour text

--- clock.js
+++ clock.js
@@ -14,28 +14,53 @@ function getSolarDeclination(now = new Date) {
 
 class Clock {
   constructor({
-    hour: {
-      hand_color = '#fff',
-      hand_width = 3.5,
-      direction_switch = false,
-      marker: {
-        dot_size = 1,
-        dot_depth = 16,
-        dot_split = 24,
-        text_depth = 20,
-        text_split = 4,
-        is24h = false, // 24|12
-      } = {},
+    hour = {},
+    minute: {
+      show_hand = false,
+      show_text = false,
+      seconds_ticker = true,
     } = {},
     offset = 0,
     $container = document.querySelector('#clock'),
-    second_ticker = true,
     second_ripple = false,
+    second_ripple_color = '#f00',
     location_marker = true,
     sun_marker = true,
     fixed_sun = true,
   } = {}){
+    this.configureHour(hour)
+    this.minute = {
+      show_hand, show_text, seconds_ticker,
+      $hand: null, $text: null,
+    }
+    this.offset = offset
+    this.second_ripple = second_ripple
+    this.second_ripple_color = second_ripple_color
+    this.location_marker = location_marker
+    this.sun_marker = sun_marker
+    this.fixed_sun = fixed_sun
+    this.$container = $container
+    this.position = null
+  }
+
+  configureHour({
+    show_text = true,
+    hand_color = '#fff',
+    hand_width = 3.5,
+    direction_switch = false,
+    marker: {
+      dot_size = 1,
+      dot_depth = 16,
+      dot_split = 24,
+      text_depth = 20,
+      text_split = 4,
+      is24h = false,
+      show_minutes = true,
+      seconds_ticker = true,
+    } = {},
+  } = {}) {
     this.hour = {
+      show_text,
       hand_color,
       hand_width,
       direction_switch,
@@ -48,16 +73,10 @@ class Clock {
         text_depth,
         text_split,
         is24h,
+        show_minutes,
+        seconds_ticker,
       },
     }
-    this.offset = offset
-    this.second_ripple = second_ripple
-    this.second_ticker = second_ticker
-    this.location_marker = location_marker
-    this.sun_marker = sun_marker
-    this.fixed_sun = fixed_sun
-    this.$container = $container
-    this.position = null
   }
 
   _hToA = hours => 360 / 24 * hours
@@ -65,24 +84,34 @@ class Clock {
   _nDigits = (v, n=2) => ('0000000'+Math.floor(v)).slice(-n)
 
   _renderHourMarkers(now = new Date) {
-    const hours = now.getHours();
+    const hours = now.getHours()
+    const minutes = now.getMinutes()
+    const seconds = now.getSeconds()
     const stroke_width = .5
-    const { dot_split, text_split, dot_depth, text_depth, dot_size, is24h } = this.hour.marker
+    const { dot_split, text_split, dot_depth, text_depth, dot_size, is24h, show_minutes, seconds_ticker } = this.hour.marker
     return `
       <g id="hour-markers" fill="#fff" stroke="#000">
         ${[...Array(24)].map((_, _h) => {
           const isCurrent = _h === hours
+          if (isCurrent && !this.hour.show_text) {
+            return ''
+          }
           const rotation = this._getHourTheta(_h)
+          let hour_text = is24h ? this._nDigits(_h) : this._nDigits(_h%12 || 12)
+          if (isCurrent && seconds_ticker) {
+            hour_text += `<tspan class="tick ${seconds%2 ?'on':''}">:</tspan>`
+          }
+          if (isCurrent && show_minutes) {
+            hour_text += `<tspan class="minutes">${this._nDigits(minutes)}</tspan>`
+          }
+          if (!is24h && _h > 11) {
+            hour_text += '<tspan class="pm">p</tspan>'
+          }
           let text = '', dot = ''
           if (isCurrent || text_split && _h%(24/text_split) === 0) {
             text = `
               <g class="hour-marker-text ${isCurrent ? 'current-hour' : ''}" style="transform:rotate(${rotation}deg) translate(0, ${100-text_depth}px);">
-                <text style="transform:rotate(${-rotation}deg) ${isCurrent ? 'translate(-6px, 5px)' : 'translate(-4px, 8px)'};">
-                  ${is24h
-                    ? this._nDigits(_h)
-                    : this._nDigits(_h%12||12)+(_h>11?'<tspan>p</tspan>':'')
-                  }
-                </text>
+                <text style="transform:rotate(${-rotation}deg) ${isCurrent ? `translate(${show_minutes ? -13 : -6}px, 5px)` : 'translate(-4px, 8px)'};">${hour_text}</text>
               </g>
             `
           }
@@ -111,6 +140,9 @@ class Clock {
   }
 
   _renderMinuteHand({minutes=0} = {}) {
+    if(!this.minute.show_hand) {
+      return ''
+    }
     const rotation = this._mToA(minutes)
     const width = 1
     const height = 100
@@ -121,8 +153,11 @@ class Clock {
   }
 
   _renderMinuteText({ minutes = 0, seconds = 0 } = {}) {
+    if(!this.minute.show_text) {
+      return ''
+    }
     const rotation = this._mToA(minutes)
-    const tick = this.second_ticker ? (seconds % 2 ? '&nbsp;' : ':') : ''
+    const tick = this.minute.seconds_ticker ? (seconds % 2 ? '&nbsp;' : ':') : ''
     const text_offset = !!tick + 2
     const color = '#fff'
     const depth = 96
@@ -203,7 +238,6 @@ class Clock {
     const location_marker_day_highlight = num_hours > 6.5 && num_hours < 17.5 ? 'filter: drop-shadow(0px 0px 0.2px #fff) drop-shadow(0px 0px 0.2px #fff);' : ''
     // <circle cx="0" cy="0" r="100" fill="#ccc"/>
     const globe_size = 170
-    const ripple_color = '#f00'
     this.$container.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="-100 -100 200 200">
         <defs>
@@ -225,9 +259,12 @@ class Clock {
           <circle cx="0" cy="0" r="${globe_size / 2}" fill="url(#clock-image-no-sea)" style="filter:grayscale(1) contrast(0) brightness(0);/*filter: grayscale(1) contrast(1.8) brightness(1);*/"></circle>
           <circle cx="0" cy="0" r="${globe_size / 2}" fill="url(#clock-image)" mask="url(#myMask)"></circle>
         </g>
-        <circle cx="0" cy="0" r="3" fill="#0000" id="second-ripple" class="${this.second_ripple ? 'active' : ''}" style="stroke:${ripple_color}"></circle>
+        <circle cx="0" cy="0" r="3" fill="#0000" id="second-ripple" class="${this.second_ripple ? 'active' : ''}" style="stroke:${this.second_ripple_color}"></circle>
         <g>${this._renderHourMarkers(now)}</g>
         ${this._renderMinuteHand({minutes})}
+        <g class="globe-rotate"><g id="sun-marker-rotate"><g id="sun-marker-translate">
+          ${this.sun_marker ? '<circle id="sun" cx="0" cy="0" r="3"></circle>' : ''}
+        </g></g></g>
         <g class="globe-rotate"><g id="location-marker-rotate"><g id="location-marker-translate"><g style="transform:scale(1.5);${location_marker_day_highlight}" fill="#f00" stroke="#f00" stroke-width=".2">
           ${
             this.location_marker ?
@@ -240,9 +277,6 @@ class Clock {
             : ''
           }
         </g></g></g></g>
-        <g class="globe-rotate"><g id="sun-marker-rotate"><g id="sun-marker-translate">
-          ${this.sun_marker ? '<circle id="sun" cx="0" cy="0" r="3"></circle>' : ''}
-        </g></g></g>
         ${this._renderHourHand({hours: num_hours})}
         <g>${this._renderMinuteText({ minutes, seconds })}</g>
       </svg>
@@ -260,8 +294,8 @@ class Clock {
 
     this.hour.$markers = this.$container.querySelector('#hour-markers')
     this.hour.$hand = this.$container.querySelector('#hour-hand')
-    this.$minute_hand = this.$container.querySelector('#minute-hand')
-    this.$minute_text = this.$container.querySelector('#minute-text')
+    this.minute.$hand = this.$container.querySelector('#minute-hand')
+    this.minute.$text = this.$container.querySelector('#minute-text')
     this.$location_marker_rotate = this.$container.querySelector('#location-marker-rotate')
     this.$location_marker_translate = this.$container.querySelector('#location-marker-translate')
     this.$sun_marker_rotate = this.$container.querySelector('#sun-marker-rotate')
@@ -279,9 +313,6 @@ class Clock {
   }
 
   reDraw(now=new Date) {
-    // if (!this.$minute_hand || this.hour.$hand) {
-    //   return this.draw()
-    // }
     const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
@@ -290,13 +321,19 @@ class Clock {
     const minute_theta = this._mToA(minutes)
     const hour_theta = this._getHourTheta(num_hours)
 
-    this.$minute_hand.style.transform = `rotate(${minute_theta+180}deg)`
-    this.hour.$hand.style.transform = `rotate(${hour_theta}deg)`
+    if(this.minute.$hand) {
+      this.minute.$hand.style.transform = `rotate(${minute_theta+180}deg)`
+    }
+    if(this.minute.$text) {
+      this.minute.$text.parentElement.innerHTML = this._renderMinuteText({ minutes, seconds })
+      this.minute.$text = this.$container.querySelector('#minute-text')
+    }
 
-    this.$minute_text.parentElement.innerHTML = this._renderMinuteText({ minutes, seconds })
-    this.$minute_text = this.$container.querySelector('#minute-text')
-    this.hour.$markers.parentElement.innerHTML = this._renderHourMarkers(now)
-    this.hour.$markers = this.$container.querySelector('#hour-markers')
+    this.hour.$hand.style.transform = `rotate(${hour_theta}deg)`
+    if (this.hour.marker.seconds_ticker || seconds < 3) {
+      this.hour.$markers.parentElement.innerHTML = this._renderHourMarkers(now)
+      this.hour.$markers = this.$container.querySelector('#hour-markers')
+    }
 
     if(seconds < 3) { // Just in case 0 was missed
       this._updateSunPosition(now)
@@ -311,12 +348,18 @@ const clock = new Clock({
     direction_switch: true,
     hand_width: 1,
     hand_color: 'yellow',
-    marker: { text_split: 4, dot_split:12, is24h:true }
+    marker: {
+      text_split: 4, dot_split: 24,
+      is24h: true,
+      // show_minutes: false,
+      seconds_ticker: false,
+    }
   },
   // second_ripple: true,
-  location_marker: false,
+  second_ripple_color: '#f0f',
+  // location_marker: false,
   offset: 0,
-  // second_ticker: false,
+  // minute: { seconds_ticker: false },
 })
 // const clock = new Clock
 clock.draw()
